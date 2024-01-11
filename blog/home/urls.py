de1bb022410feb21dfb538fafabff87c70aa96e8
yua-oss <1412456353@qq.com>
2024-01-11 09:41:45 +0800

    '博客保存+详情展示'

--- blog/home/urls.py
+++ blog/home/urls.py
@@ -1,8 +1,10 @@
 # 2.6展示首页
 from django.urls import path
-from home.views import IndexView
+from home.views import IndexView, DetailView
 
 # 2.6展示首页
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    # 8.1 详情视图的路由
+    path('detail/', DetailView.as_view(), name='detail'),
 ]
